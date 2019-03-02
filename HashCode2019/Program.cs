889800c6236d1ce36b4fce93061ef4b17c5092c0
NataliVol4ica <natvol4ica@gmail.com>
2019-03-02 19:56:56 +0200

    untested full chain search

--- HashCode2019/Program.cs
+++ HashCode2019/Program.cs
@@ -22,29 +22,33 @@ namespace HashCode2019
             public State state;
             public int totalInterest;
             public int parent;
+            public bool isTaken;
             public VertexData(int index, State state, int totalInterest, int parent)
             {
+                this.isTaken = false;
                 this.index = index;
                 this.state = state;
                 this.totalInterest = totalInterest;
                 this.parent = parent;
             }
-            public int CompareTo(VertexData right)
+            public int CompareTo(VertexData right) //???
             {
+                if (index == right.index)
+                    return 0;
                 int cmp = totalInterest - right.totalInterest;
                 if (cmp == 0)
                     cmp = index - right.index;
                 return cmp;
             }
+            public override string ToString()
+            {
+                return index + " " + state + " " + totalInterest;
+            }
         }
 
-        public static List<VertexData> GetVertexDataWithMaxProfitValues(int first, LinkData linkData)
+        public static List<VertexData> GetVertexDataWithMaxProfitValues(int first, LinkData linkData, List<VertexData> vertexDatas)
         {
-            int i = 0;
-            var vertexDatas = Enumerable.Repeat(
-                new VertexData
-                (i++, State.unvisited, 0, -1), linkData.VertexNum)
-                .ToList();
+          
             VertexData currentVertex;
             var queue = new SortedSet<VertexData>();
             queue.Add(vertexDatas[first]);
@@ -52,13 +56,15 @@ namespace HashCode2019
             while (queue.Count > 0)
             {
                 currentVertex = queue.Max();
-                Console.WriteLine("Current {0} Max {1} Parent {2} | Queue size {3}",
-                    currentVertex.index, currentVertex.totalInterest, currentVertex.parent,
-                    queue.Count);
+                if (queue.Count % 100 == 0)
+                    Console.WriteLine("Current {0} Max {1} Parent {2} | Queue size {3}",
+                        currentVertex.index, currentVertex.totalInterest, currentVertex.parent,
+                        queue.Count);
                 foreach (var neighbour in linkData.IntLinks[currentVertex.index])
                 {
-                    Console.WriteLine("Neighbour {0}", neighbour.slideIndex);
-                    if (vertexDatas[neighbour.slideIndex].state == State.visited)
+                    //Console.WriteLine("Neighbour {0}", neighbour.slideIndex);
+                    if (vertexDatas[neighbour.slideIndex].isTaken|| 
+                        vertexDatas[neighbour.slideIndex].state == State.visited)
                         continue;
                     if (vertexDatas[neighbour.slideIndex].state == State.unvisited)
                     {
@@ -68,95 +74,101 @@ namespace HashCode2019
                     if (vertexDatas[currentVertex.index].totalInterest + neighbour.interest >
                         vertexDatas[neighbour.slideIndex].totalInterest)
                     {
+                        queue.Remove(vertexDatas[neighbour.slideIndex]);
                         vertexDatas[neighbour.slideIndex].totalInterest =
                             vertexDatas[currentVertex.index].totalInterest + neighbour.interest;
+                        queue.Add(vertexDatas[neighbour.slideIndex]);
                         vertexDatas[neighbour.slideIndex].parent = currentVertex.index;
                     }
                 }
-                queue.Remove(currentVertex);
+                if (!(queue.Remove(currentVertex)))
+                {
+                    Console.WriteLine(">>>>>>>>>>>> Beda <<<<<<<<<<<<<");
+                }
                 vertexDatas[currentVertex.index].state = State.visited;
             }
             return vertexDatas;
         }
-        public static List<int> FindBestPath(int first, LinkData linkData)
+        public static List<int> FindBestPath(int first, LinkData linkData, List<VertexData> vertexDatas)
         {
             Console.WriteLine("Searching path for {0}", first);
             var ans = new List<int>();
-            var vertexDatas = GetVertexDataWithMaxProfitValues(first, linkData);
-            int curVertex = 0;
-            int max = vertexDatas[0].totalInterest;
-            for (int i = 1; i < vertexDatas.Count; i++)
+            vertexDatas = GetVertexDataWithMaxProfitValues(first, linkData, vertexDatas);
+            int curVertex = -1;
+            int max = -1;
+            for (int i = 0; i < vertexDatas.Count; i++)
                 if (vertexDatas[i].totalInterest > max)
                 {
                     curVertex = i;
                     max = vertexDatas[i].totalInterest;
                 }
             ans.Add(curVertex);
+            vertexDatas[curVertex].isTaken = true;
             while (curVertex != first)
             {
                 curVertex = vertexDatas[curVertex].parent;
                 ans.Add(curVertex);
+                vertexDatas[curVertex].isTaken = true;
             }
             ans.Reverse();
             Console.WriteLine("Search complete.");
             return ans;
         }
-        public static void GreedyAlgo(LinkData linkData)
+        public static List<int> GreedyAlgo(LinkData linkData)
         {
-            //order vertexes by neighbours number. +
-            //get first unadded
-            //depth search. Find the biggest profit || most farther vertex.
-            //[expensive improvement]find the least busy path to there
-            //from pathes of fixed length by returning back in generations
-            //remember the path, mark path vertexes as busy
+            var vertexDatas = new List<VertexData>();
+            for (int j = 0; j < linkData.VertexNum; j++)
+                vertexDatas.Add(new VertexData(j, State.unvisited, 0, -1));
             //if pow(vertex) > 2 do another width search and concat two pathes
-            var checkd = Enumerable.Repeat(false, linkData.VertexNum).ToList();
             int i = 0;
-            int curIdx = 0;
+            int untaken = 0;
+            var ans = new List<int>();
             while (true)
             {
                 //find first unchecked vertex
                 while (i < linkData.VertexNum)
                 {
-                    if (!checkd[i])
+                    if (!vertexDatas[i].isTaken)
                     {
-                        curIdx = i;
+                        untaken = i;
                         break;
                     }
                     i++;
                 }
-                //no unchecked vertexes left
+                //if no unchecked vertexes left
                 if (i == linkData.VertexNum)
                     break;
                 //if vertex has no profitable neighbours
-                if (linkData.Repeats[curIdx].amount < 1)
+                if (linkData.Repeats[untaken].amount < 1)
                 {
-                    checkd[curIdx] = true;
+                    vertexDatas[untaken].isTaken = true;
                     i++;
                     continue;
                 }
-                checkd[curIdx] = true; //antiendlesscycle
-                //find one chain
-                //mark as checked
-                //if more than 1 path comes through this vertex
-                //if (linkData.Repeats[curIdx].amount > 1)
-                //call profitCount again
-                //mark as checked
-                //combine pathes
-                //save result to the answer list
+                //find first chain
+                var oneChain = FindBestPath(untaken, linkData, vertexDatas);
+                if (linkData.Repeats[untaken].amount == 1)
+                {
+                    ans.AddRange(oneChain);
+                    i++;
+                    continue;
+                }
+                //if it has two ways, reverse it
+                oneChain.Reverse();
+                oneChain.RemoveAt(oneChain.Count - 1);
+                vertexDatas[untaken].isTaken = false;
+                //and find another chain
+                var secondchain = FindBestPath(untaken, linkData, vertexDatas);
+                ans.AddRange(secondchain);
             }
             Console.WriteLine("Finished");
+            return ans;
         }
         static void Main()
         {
             var linkData = new LinkData(DataAnalyzer.linksB, 80000);
-            //GreedyAlgo(linkData);
-            var anyAns = FindBestPath(0, linkData);
-            Tools.SaveIntAnswer(anyAns, DataAnalyzer.ansB);
-            //var test = new SortedSet<IntLinkData>();
-            //test.Add(new IntLinkData(1, 1));
-            //test.Add(new IntLinkData(1, 2));
-
+            var result = GreedyAlgo(linkData);
+            Tools.SaveIntAnswer(result, DataAnalyzer.ansB);
             Console.WriteLine("Press any key");
             Console.Read();
         }
